1526faf7f0cc58984a268d1fddedc69644067f5e
pedro <pedro9@github.com>
2018-07-31 23:49:02 -0300

    completed data processing stage

--- process_data.py
+++ process_data.py
@@ -1,8 +1,5 @@
 # coding: utf-8
-
 import pandas
-import numpy as np
-import datetime
 import wbdata
 
 def get_indicator(x, y):
@@ -12,14 +9,15 @@ def get_indicator(x, y):
     df.rename(index=str, columns={"country": "Country", "date": "Year"}, inplace=True)
     df['Year'] = df['Year'].apply(lambda x : int(x.year))
     df = df.pivot(index='Country', columns='Year', values=y)
-    df = df.loc[:,'1990':'2016'].dropna(axis='rows')
+    df = df.loc[:, '1990':'2016'].dropna(axis='rows')
     return df
 
 
 def get_regions():
     ct = wbdata.get_country(display=False)
-    regions = pandas.DataFrame([(i['id'],i['name'], i['region'].get('value')) for i in ct],
-                             columns = ['ID','Countries','Group'])
+    regions = pandas.DataFrame([(i['id'], i['name'], i['region'].get('value'))
+                              for i in ct],
+                              columns=['ID', 'Countries', 'Group'])
 
     regions.index = regions.Countries
     regions.drop('Countries', axis=1, inplace=True)
@@ -27,12 +25,23 @@ def get_regions():
 
 
 def process_data():
-    x_dim      = get_indicator(x="NY.GDP.PCAP.PP.KD", y="gdp_pc")
-    y_dim      = get_indicator(x="UNEMPSA_", y="unemployment")
+    x_dim = get_indicator(x="NY.GDP.PCAP.PP.KD", y="gdp_pc")
+    y_dim = get_indicator(x="UNEMPSA_", y="unemployment")
     bubble_dim = get_indicator(x="TX.VAL.TECH.MF.ZS", y="tech_exp")
     years = list(x_dim.columns)
+    #main_countries=list(x_dim.index[:15])
+    a=list(x_dim.index)
+    b=list(y_dim.index)
+    c=list(bubble_dim.index)
+    main_countries=list(set(a) & set(b) & set(c))
 
-    regions = get_regions()
-    regions_list = list(regions.Group.unique())
+    x_dim = x_dim[x_dim.index.isin(main_countries)]
+    y_dim = y_dim[y_dim.index.isin(main_countries)]
+    bubble_dim = bubble_dim[bubble_dim.index.isin(main_countries)]
 
-    return x_dim, y_dim, bubble_dim, regions, years, regions_list, ["gdp_pc", "unemployment_rate", "tech_export"] 
+    regions_df = get_regions()
+    regions_list = list(regions_df.Group.unique())
+
+    return x_dim, y_dim, bubble_dim, regions_df, years, regions_list, ["gdp_pc", 
+                                                                    "unemployment_rate", 
+                                                                    "tech_export"] 
--- process_data2.py
+++ process_data2.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 # -*- coding: utf-8 -*-
 import pandas as pd
 
@@ -14,22 +20,77 @@ from bokeh.plotting import figure
 from process_data import process_data
 
 
+# In[2]:
+
+
 #fertility_df, life_expectancy_df, population_df_size, regions_df, years, regions_list = process_data()
 x_dim, y_dim, bubble_dim, regions_df, years, regions_list, dims = process_data()
 
+
+# In[3]:
+
+
+print(dims[0])
+print("-"*60)
+print(x_dim.head(), x_dim.shape)
+print("/"*50)
+print(dims[1])
+print("-"*60)
+print(y_dim.head(), y_dim.shape)
+print("/"*50)
+print(dims[2])
+print("-"*60)
+print(bubble_dim.head(), bubble_dim.shape)
+print("/"*50)
+
+
+# In[4]:
+
+
 p = pd.Panel({dims[0]: x_dim, dims[1]: x_dim, dims[2]: bubble_dim})
+
+
+# In[8]:
+
+
+x_dim.index
+
+
+# In[13]:
+
+
+regions_df[regions_df.index.isin(list(x_dim.index))].Group.name
+
+
+# In[14]:
+
 
 data = {}
 
+
+# In[15]:
+
+
 region_name = regions_df[regions_df.index.isin(list(x_dim.index))].Group
 region_name.name = 'region'
+
+
+# In[16]:
+
 
 for year in years:
     df = pd.concat([p.loc[:, :, year], region_name], axis=1).reset_index()
     data[year] = df.to_dict('series')
 
+
+# In[17]:
+
+
 print('saved')
 source = ColumnDataSource(data=data[years[0]])
+# Palette length does not match number of factors. ['East Asia & Pacific', 'North America'] will be assigned to `nan_color` gray
+# warnings.warn("Palette length does not match number of factors. %s will be assigned to `nan_color` %s" % (extra_factors, self.nan_color))
+
 
 plot = figure(x_range=(245, 136000), y_range=(0.5, 26.5), title='Gapminder Data', plot_height=300)
 plot.xaxis.ticker = SingleIntervalTicker(interval=15000)
@@ -92,3 +153,4 @@ layout = layout([
 
 curdoc().add_root(layout)
 curdoc().title = "Gapminder"
+
